2acd7a10eb1a52069ec6d6452c5635923f86592b
Anthony Achonye <tonexify@gmail.com>
2017-09-10 18:59:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowercase()
+}
+
+function logShout(string) {
+  if (string.toUpperCase() === string) {
+    console.log(string)
+  }
+}
+
+function logWhisper(string) {
+  if (string.toLowercase() === string) {
+    console.log(string)
+  }
+}
+
+function sayHiToGrandma (string) {
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string.toLowercase() === string) {
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
